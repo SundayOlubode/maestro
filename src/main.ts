83471e5b4212656229d73ee6a89eba37fff7a9dd
Sam <s.olubode@alustudent.com>
2025-03-25 02:08:13 +0100

    update: cors

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,13 @@ import { join } from 'path';
 async function bootstrap() {
   const app =
     await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors({
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
+
   app.useStaticAssets(join(__dirname, '..', 'email'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
