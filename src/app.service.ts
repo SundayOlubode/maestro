b929ffdcd155bcfc09b162d047f1e573a815ac3c
Sam <s.olubode@alustudent.com>
2025-04-15 18:30:04 +0100

    update

--- src/app.service.ts
+++ src/app.service.ts
@@ -12,6 +12,12 @@ export class AppService {
     await this.wordService.sendWordUsagesToUsers();
   }
 
+  @Cron('0 45 11 * * *') // Every day at 6:50 AM (4:50 PM UTC - Server time)
+  async handleDailyNoonWordUsageNotification() {
+    console.log('Sending word usages to users...');
+    await this.wordService.sendWordUsagesToUsers();
+  }
+
   @Cron('0 20 20 * * *') // Every day at 10:50 PM (20:20 PM UTC - Server time)
   async handleDailyEveningWordUsageNotification() {
     console.log('Sending evening word usages to users...');
